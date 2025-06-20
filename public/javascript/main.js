d237daee5512673ab26fabce6a992ef2b81ad5ab
Guilherme Vinicius de Oliveira Pereira <112845783+graple-guillos@users.noreply.github.com>
2025-06-20 20:11:53 -0300

    Update main.js - adding some sense into the code
    When you're young, you do dumb things. You're reckless. You think you're the smartest kid inside the freaking room. It turns out you are not.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -13,28 +13,10 @@ submitBtn.disabled = true
 const choosenRating = document.querySelector('.orange-letters')
 const rating = document.querySelectorAll('.number-button')
 
-rating[0].addEventListener('click', function(){
-  choosenRating.textContent = 'You selected 1 out of 5'
-  submitBtn.disabled = false
-})
-
-rating[1].addEventListener('click', function(){
-  choosenRating.textContent = 'You selected 2 out of 5'
-  submitBtn.disabled = false
-})
-
-rating[2].addEventListener('click', function(){
-  choosenRating.textContent = 'You selected 3 out of 5'
-  submitBtn.disabled = false
-})
-
-rating[3].addEventListener('click', function(){
-  choosenRating.textContent = 'You selected 4 out of 5'
-  submitBtn.disabled = false
-})
-
-rating[4].addEventListener('click', function(){
-  choosenRating.textContent = 'You selected 5 out of 5'
-  submitBtn.disabled = false
+rating.forEach((rt) => {
+  rt.addEventListener('click', () => {
+    choosenRating.textContent = `You selected ${rt.textContent} out of 5`
+    submitBtn.disabled = true
+  })
 })
 
